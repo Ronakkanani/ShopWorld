a0b6a80f1dcf7845eaf4fd8dacd5c72621dced25
Ronakkanani <127604771+Ronakkanani@users.noreply.github.com>
2024-03-14 08:46:01 +0530

    "change to logo name"

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -10,7 +10,7 @@ export default function Header({ setSearch }) {
         <>
             <nav className="navbar sticky-top navbar-expand-lg navbar-light headerMain bg-white shadow-sm py-3">
                 <div className="container-fluid">
-                    <Link className='nav-link' to={`/`}><a className="navbar-brand m-0 logo" href="#">Shopping App</a></Link>
+                    <Link className='nav-link' to={`/`}><a className="navbar-brand m-0 logo" href="#">Shopping app</a></Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
